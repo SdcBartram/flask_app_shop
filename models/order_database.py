18ad1523ada3ac5906e84dc196975c9e96fbf26d
Sam <sdcbartram@icloud.com>
2023-04-20 11:07:39 +0100

    updates after solution recap

--- models/order_database.py
+++ models/order_database.py
@@ -5,3 +5,14 @@ order_02 = Order("Annie Ant", "27/03/2023", "Always Available Antiques", "Anne T
 order_03 = Order("Calvin Cooke", "28/03/2023", "Colourful Collections", "Cobalt Chesterton", 1)
 
 orders = [order_01, order_02, order_03]
+
+# from solution ---> date/time format
+
+# from models.order import *
+# import datetime
+
+# order1 = Order(datetime.date(2020, 5, 17), "Luke Skywalker", 1,  'Pepperoni')
+# order2 = Order(datetime.date(2020, 6, 18), "Han Solo", 2, "Quatro Fromagio")
+# order3 = Order(datetime.date(2020, 7, 19), "Darth Vader", 1, "Pineapple")
+
+# orders = [order1, order2, order3]
