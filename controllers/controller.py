65ccda80508c7fe5fd4e8fa1ae5db9a6fa6d8c63
Sam <sdcbartram@icloud.com>
2023-04-20 11:15:07 +0100

    format saving

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,11 +3,13 @@ from flask import render_template
 
 from models.order_database import orders
 
+
 @app.route('/orders')
 def index():
-    return render_template('index.jinja', orders = orders)
+    return render_template('index.jinja', orders=orders)
+
 
 @app.route('/orders/<index>')
 def display_order(index):
-    resulting_order=orders[int(index)]
+    resulting_order = orders[int(index)]
     return render_template('order.jinja', order=resulting_order)
